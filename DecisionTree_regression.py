0254b768f83e3b3574c4b958b9b2fdb489e99b1f
xclovehsy <2505327613@qq.com>
2022-11-16 19:02:29 +0800

    regression

--- DecisionTree_regression.py
+++ DecisionTree_regression.py
@@ -4,16 +4,24 @@
 # datetime: 2022-11-04 12:05
 # software: PyCharm
 import math
-import numpy as np
-from copy import deepcopy
+import sys
+
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.preprocessing import StandardScaler
 import plottree
 import pickle
+from sklearn.metrics import accuracy_score, r2_score
+import matplotlib.pyplot as plt
 
 
 class DT:
+
+    def __init__(self, depth):
+        self.depth = depth
+
     def postPruning(self, dtree, subtree, X, y):
         """
         dfs算法对决策树进行后剪枝
@@ -26,12 +34,15 @@ class DT:
         # 叶子节点不用剪枝
         if type(subtree).__name__ != "dict":
             return
+
         # dfs遍历左右节点
         key = next(iter(subtree))
         self.postPruning(dtree, subtree[key]["left"], X, y)
         self.postPruning(dtree, subtree[key]["right"], X, y)
-        # 不剪枝前决策树的准确性
-        old_acc = self.getTreeAcc(dtree, X, y)
+
+        # 不剪枝前决策树的代价
+        old_cost = self.getTreeCost(dtree, X, y)
+
         # 对subtree的左子树进行剪枝处理
         sides = ["left", "right"]
         for side in sides:
@@ -39,20 +50,23 @@ class DT:
                 # 获取左子树的所有叶节点
                 leaves = []
                 self.getSubTreeLeaf(subtree[key][side], leaves)
+
                 temp_subtree = subtree[key][side]
                 best_leaf = leaves[0]
-                best_acc = 0.0
+                min_cost = sys.maxsize
                 for leaf in leaves:
                     # 预剪枝
                     subtree[key][side] = leaf
-                    new_acc = self.getTreeAcc(dtree, X, y)
-                    if new_acc >= best_acc:
-                        best_acc = new_acc
+                    new_cost = self.getTreeCost(dtree, X, y)
+                    if new_cost <= min_cost:
+                        min_cost = new_cost
                         best_leaf = leaf
+
                     # 恢复原状
                     subtree[key][side] = temp_subtree
+
                 # 若剪枝后的准确性提高则进行剪枝处理
-                if best_acc >= old_acc:
+                if min_cost <= old_cost:
                     subtree[key][side] = best_leaf
 
     def getSubTreeLeaf(self, tree, leaves):
@@ -71,16 +85,17 @@ class DT:
         self.getSubTreeLeaf(tree[key]["left"], leaves)
         self.getSubTreeLeaf(tree[key]["right"], leaves)
 
-    def getTreeAcc(self, dtree, X, y):
+    def getTreeCost(self, dtree, X, y):
         """
-        获取决策树的精度
+        获取决策树的cost
         :param dtree: 决策树
         :param X: 样本特征
         :param y: 正确标签
         :return:
         """
-        pre_y = self.predict(dtree, X)
-        return sum(pre_y == y) / len(pre_y)
+        pre_y = np.array(self.predict(dtree, X))
+        cost = np.dot(pre_y - y, (pre_y - y).T)
+        return cost
 
     def predict(self, dtree, X):
         """
@@ -104,16 +119,19 @@ class DT:
         """
         if type(dtree).__name__ != "dict":
             return dtree
+
         # 获取决策树节点
         key = next(iter(dtree))
         sub_tree = dtree[key]
+
         feature_index, divide_value = key[1], key[2]
+
         if X[feature_index] >= divide_value:
             return self.getPredict(sub_tree["right"], X)
         else:
             return self.getPredict(sub_tree["left"], X)
 
-    def createDecTree(self, X, y, id2name):
+    def createDecTree(self, X, y, id2name, depth):
         """
         创建决策树函数
         :param id2name:
@@ -122,48 +140,44 @@ class DT:
         :return:
         """
         labels = list(set(y))
+        if depth > self.depth:
+            return self.getAvgValue(y)
+
         # 若当前数据集中只有一种label的样本，则直接返回唯一的label
         if len(labels) == 1:
             return labels[0]
+
         # 只有一种属性且所有属性值相同
         if len(X[0]) == 1 and len(list(set(X[:, 0]))) == 1:
-            return self.getMostLabel(y)
+            return self.getAvgValue(y)
+
         # 若当前数据集中无特征，则返回最多的样本label
         if len(X[0]) == 0:
-            return self.getMostLabel(y)
+            return self.getAvgValue(y)
 
         # 当前最优的feature
-        best_feature, feature_divide_value, gain_feature = self.getBestFeature(X, y)
+        best_feature, feature_divide_value, cost = self.getBestFeature(X, y)
+
         # 构建当前数据下的决策树
-        key = (id2name[best_feature], best_feature, feature_divide_value, gain_feature)
+        key = (id2name[best_feature], best_feature, feature_divide_value, cost)
         deTree = {key: {}}
+
         # 获取大于value以及小于value的子数据集
         sub_X_left, sub_y_left, sub_X_right, sub_y_right = self.spiltDataSetByFeature(X, y, best_feature,
                                                                                       feature_divide_value)
-        deTree[key]["left"] = self.createDecTree(sub_X_left, sub_y_left, id2name)
-        deTree[key]["right"] = self.createDecTree(sub_X_right, sub_y_right, id2name)
+
+        deTree[key]["left"] = self.createDecTree(sub_X_left, sub_y_left, id2name, depth+1)
+        deTree[key]["right"] = self.createDecTree(sub_X_right, sub_y_right, id2name, depth+1)
+
         return deTree
 
-    def getMostLabel(self, labels):
+    def getAvgValue(self, y):
         """
-        获取数量最多的label
-        :param labels:
+        获取当前数据集的平均值
+        :param y:
         :return:
         """
-        num_label = {}
-        # 统计每种样本的数量
-        for label in labels:
-            if label not in num_label.keys():
-                num_label[label] = 0
-            num_label[label] += 1
-        most_label = ""
-        max_cnt = 0
-        for label in num_label.keys():
-            if num_label[label] > max_cnt:
-                max_cnt = num_label[label]
-                most_label = label
-
-        return most_label
+        return np.average(y)
 
     def getBestFeature(self, X, y):
         """
@@ -177,40 +191,43 @@ class DT:
         cnt_sample = len(X)
 
         # 计算数据集原始的信息熵
-        base_Ent = self.calcDataEnt(X, y)
-
-        max_gain = 0.0
         best_feature = 0
         best_feature_divide_value = 0
+        min_feature_cost = sys.maxsize
+
         # 计算每个属性的信息增益
         for i in range(len(X[0])):
             # 获取当前属性的取值
-            feature_values = list(set(X[:, i]))
+            feature_values = np.sort(np.array(list(set(X[:, i]))))
+
+            # 获取切分的所有值
             divide_values = []
             for j in range(len(feature_values) - 1):
                 divide_values.append((feature_values[j] + feature_values[j + 1]) / 2)
 
-            max_gain_col = 0.0
+            min_cost = sys.maxsize
             feature_divide_value = 0
+
             # 对于每个属性取值计算对应的信息熵
             for value in divide_values:
-                # 当前属性的信息增益
-                gain = base_Ent
                 # 按照属性的取值获取子数据集
                 sub_X_left, sub_y_left, sub_X_right, sub_y_right = self.spiltDataSetByFeature(X, y, i, value)
-                gain -= (len(sub_X_left) / cnt_sample) * self.calcDataEnt(sub_X_left, sub_y_left)
-                gain -= (len(sub_X_right) / cnt_sample) * self.calcDataEnt(sub_X_right, sub_y_right)
-                if gain >= max_gain_col:
-                    feature_divide_value = value
-                    max_gain_col = gain
 
-            # print(f"feature={i}, gain={max_gain_col}, divide_value={feature_divide_value}")
-            if max_gain_col >= max_gain:
-                max_gain = max_gain_col
+                cost = np.dot(sub_y_left - np.average(sub_y_left), (sub_y_left - np.average(sub_y_left)).T)
+                cost += np.dot(sub_y_right - np.average(sub_y_right), (sub_y_right - np.average(sub_y_right)).T)
+
+                if cost <= min_cost:
+                    min_cost = cost
+                    feature_divide_value = value
+
+            if min_cost <= min_feature_cost:
+                min_feature_cost = min_cost
                 best_feature = i
                 best_feature_divide_value = feature_divide_value
 
-        return best_feature, best_feature_divide_value, max_gain
+        # print(f"choose_feature={best_feature}, cost={min_feature_cost}, divide_value={best_feature_divide_value}")
+
+        return best_feature, best_feature_divide_value, min_feature_cost
 
     def spiltDataSetByFeature(self, X, y, feature, value):
         """
@@ -227,10 +244,10 @@ class DT:
 
         for i in range(len(X)):
             if X[i][feature] >= value:
-                sub_X_right.append(list(np.concatenate([X[i][:feature], X[i][feature + 1:]], axis=0)))
+                sub_X_right.append(X[i])
                 sub_y_right.append(y[i])
             else:
-                sub_X_left.append(list(np.concatenate([X[i][:feature], X[i][feature + 1:]], axis=0)))
+                sub_X_left.append(X[i])
                 sub_y_left.append(y[i])
 
         return np.array(sub_X_left), np.array(sub_y_left), np.array(sub_X_right), np.array(sub_y_right)
@@ -252,6 +269,7 @@ class DT:
                 num_labels[label] = 1
             else:
                 num_labels[label] += 1
+
         # 计算信息熵
         data_Ent = 0.0
         for label in num_labels.keys():
@@ -261,9 +279,9 @@ class DT:
         return data_Ent
 
 
-def iris_classify():
-    data_path = r"Dataset/iris_data.xlsx"
-    tree_path = r"DTstore/iris_dt.txt"
+def icecream_classify():
+    data_path = r"Dataset/icecream_data.xlsx"
+    tree_path = r"icecream.txt"
     df = pd.read_excel(data_path)
 
     X = pd.DataFrame(df).values[:, :-1]
@@ -276,35 +294,34 @@ def iris_classify():
         name2id[feature_name[i]] = i
 
     # 训练集和测试集切分
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    df2 = pd.DataFrame(np.hstack([y_test.reshape((len(y_test), 1)), x_test])).sort_values(0).values
+    y_test = df2[:, 0]
+    x_test = df2[:, 1:]
 
-    # 训练模型
-    dt = DT()
-    dtree = dt.createDecTree(x_train, y_train, id2name)
+    dt = DT(len(X[0])*5)
+    dtree = dt.createDecTree(x_train, y_train, id2name, 0)
 
-    # 剪枝前模型准确性评估
-    print("剪枝前决策树准确率")
-    print(classification_report(y_test, dt.predict(dtree, x_test)))
-    # 绘制决策树图像
+    print(f"剪枝前 r2={r2_score(y_test, dt.predict(dtree, x_test))}")
+    plottree.createPlot(dtree)
+    plt.figure()
+    plt.plot(range(len(y_test)), y_test, 'b-', label="tureValue")
+    plt.plot(range(len(y_test)), dt.predict(dtree, x_test), 'y-', label="beforeCut")
+
+    dt.postPruning(dtree, dtree,x_test, y_test)
+
+    print(f"剪枝后 r2={r2_score(y_test, dt.predict(dtree, x_test))}")
+    plt.plot(range(len(y_test)), dt.predict(dtree, x_test), 'r-', label="cutAfter")
+    plt.xlabel("sample")
+    plt.ylabel("icecreamPrice")
+    plt.grid()
+    plt.legend()
+    plt.show()
     plottree.createPlot(dtree)
 
-    # 后剪枝
-    dt.postPruning(dtree, dtree, x_train, y_train)
-
-    # 剪枝后模型准确性评估
-    print("剪枝后决策树准确率")
-    print(classification_report(y_test, dt.predict(dtree, x_test)))
-    # 绘制决策树图像
-    plottree.createPlot(dtree)
-
-    # 存储决策树
-    with open(tree_path, 'wb') as fw:
-        pickle.dump(dtree, fw)
-
-
-def wine_classify():
-    data_path = r"Dataset/winequality_data.xlsx"
-    tree_path = r"DTstore/wine_dt.txt"
+def housing_classify():
+    data_path = r"Dataset/housing_data.xlsx"
+    tree_path = r"housing_dt.txt"
     df = pd.read_excel(data_path)
 
     X = pd.DataFrame(df).values[:, :-1]
@@ -319,32 +336,31 @@ def wine_classify():
     # 训练集和测试集切分
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    # 训练模型
-    dt = DT()
-    dtree = dt.createDecTree(x_train, y_train, id2name)
+    df2 = pd.DataFrame(np.hstack([y_test.reshape((len(y_test), 1)), x_test])).sort_values(0).values
+    y_test = df2[:, 0]
+    x_test = df2[:, 1:]
 
-    # 模型准确性评估
-    print("剪枝前决策树准确率")
-    print(classification_report(y_test, dt.predict(dtree, x_test)))
+    dt = DT(len(X[0])*2)
+    dtree = dt.createDecTree(x_train, y_train, id2name, 0)
+    # print(dtree)
 
-    # 绘制决策树图像
-    # plottree.createPlot(dtree)
-
-    # 后剪枝
+    print(f"剪枝前模型r2={r2_score(y_test, dt.predict(dtree, x_test))}")
+    # plottree.createPlot(dtre e)
+    plt.figure(1)
+    plt.plot(range(len(y_test)), y_test, 'b-', label="tureValue")
+    plt.plot(range(len(y_test)), dt.predict(dtree, x_test), 'y-', label="beforeCut")
     dt.postPruning(dtree, dtree, x_test, y_test)
 
-    # 模型准确性评估
-    print("剪枝后决策树准确率")
-    print(classification_report(y_test, dt.predict(dtree, x_test)))
-
-    # 绘制决策树图像
-    plottree.createPlot(dtree)
-
-    # 存储决策树
-    with open(tree_path, 'wb') as fw:
-        pickle.dump(dtree, fw)
+    print(f"剪枝后模型r2={r2_score(y_test, dt.predict(dtree, x_test))}")
+    plt.plot(range(len(y_test)), dt.predict(dtree, x_test), 'r-', label="cutAfter")
+    plt.xlabel("sample")
+    plt.ylabel("housePrice")
+    plt.grid()
+    plt.legend()
+    plt.show()
+    # plottree.createPlot(dtree)
 
 
 if __name__ == '__main__':
-    iris_classify()
-    # wine_classify()
+    # icecream_classify()
+    housing_classify()
--- test.py
+++ test.py
@@ -3,19 +3,28 @@
 # author: 徐聪
 # datetime: 2022-11-07 0:11
 # software: PyCharm
+import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 # x = range(1, 27)
 # y = [1, 2, 4, 8, 16, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 21, 22, 23, 24, 25, 26, 1, 2, 4, 8]
-x = range(0, 19)
-y =[1,2,3,4,2,3,4,5,6,3,4,2,3,4,2,3,4,5,2]
+import pandas as pd
 
-plt.figure()
-plt.plot(x, y, 'ro-')
-plt.grid()
-plt.xlabel("RTT")
-plt.ylabel("cwnd")
-plt.show()
+x = range(0, 19)
+y = np.array([1,2,3,4,2,3,4,5,6,3,4,2,3,4,2,3,4,5,2])
+y2 = np.array([1,2,3,4,2,3,4,5,6,3,4,2,3,4,2,3,4,5,2])
+
+df = pd.DataFrame(np.array([y, y2]).T).sort_values(0).values
+print(df)
+# cost = np.dot(y-np.average(y), (y-np.average(y)).T)
+# print(cost)
+
+# plt.figure()
+# plt.plot(x, y, 'ro-')
+# plt.grid()
+# plt.xlabel("RTT")
+# plt.ylabel("cwnd")
+# plt.show()
 # print(sum(y[:7]))
